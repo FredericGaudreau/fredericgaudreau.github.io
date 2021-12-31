ba7d34e58de8f07e3adb52f36bd9e106a5cd6fe3
Frédéric Gaudreau <gaudreauf@gmail.com>
2021-12-31 17:53:30 -0500

    nuages de points

--- sketch.js
+++ sketch.js
@@ -6,16 +6,14 @@ var g;
 var b;
 
 function setup() {
-  createCanvas(windowWidth*.99, windowHeight*.97); // create canvas with window width and height as dimensions
-  
+  createCanvas(windowWidth * 0.99, windowHeight * 0.98); // create canvas with window width and height as dimensions
+
   // x = width / 2; // set starting x position - center of canvas
   // y = width / 2; // set starting y position - center of canvas
   // x = width / 20; // set starting x position - left side of canvas
   // y = width / 20; // set starting y position - upper side of canvas
-  x= random(0,windowWidth*.99); // set starting x position - random
-  y= random(0,windowHeight*.97); // set starting y position - random
-  // y= random(windowWidth); // set starting y position - random
-  // x= random(windowHeight); // set starting x position - random
+  x = random(0, windowWidth * 0.99); // set starting x position - random
+  y = random(0, windowHeight * 0.97); // set starting y position - random
 
   r = random(0, 255); // set starting red value
   g = random(0, 255); // set starting green value
@@ -26,12 +24,12 @@ function setup() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth*.99, windowHeight*.97); // resize canvas to window width and height
+  resizeCanvas(windowWidth * 0.99, windowHeight * 0.98); // resize canvas to window width and height
 }
 
 function draw() {
-  // randomly move line
-  var randomValue = random(); // random value between 0 and 1
+  var randomValue = random(0, 1);
+  // randomly move lines
   if (randomValue < 0.25) {
     x--;
   } else if (randomValue < 0.5) {
@@ -44,15 +42,15 @@ function draw() {
 
   // wrap around left and right sides
   if (x < 0) {
-    x = width;
-  } else if (x > width) {
+    x = windowWidth;
+  } else if (x > windowWidth) {
     x = 0;
   }
 
   // wrap around top and bottom sides
   if (y < 0) {
-    y = height;
-  } else if (y > height) {
+    y = windowHeight;
+  } else if (y > windowHeight) {
     y = 0;
   }
 
@@ -70,4 +68,9 @@ function draw() {
 
   point(x, y); // draw point
   point(y, x); // draw point (mirrored)
+
+  // nuage de points randoms
+
+  point(random(0, windowWidth), random(0, windowHeight)); // draw point (random)
+  point(random(0, windowHeight), random(0, windowWidth)); // draw point (random)
 }
