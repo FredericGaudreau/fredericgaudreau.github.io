44610a4588b4d48dfbbde296acff5f76ef1ab51c
Frédéric Gaudreau <gaudreauf@gmail.com>
2022-05-01 17:45:08 -0400

    slightly better..

--- sketch.js
+++ sketch.js
@@ -5,15 +5,10 @@ var r;
 var g;
 var b;
 
+// let t = 0; // time variable
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  // x = windowWidth / 2; // set starting x position - center of canvas
-  // y = windowHeight / 2; // set starting y position - center of canvas
-  // x = width / 20; // set starting x position - left side of canvas
-  // y = width / 20; // set starting y position - upper side of canvas
-  // x = random(0, windowWidth * 0.99); // set starting x position - random
-  // y = random(0, windowHeight * 0.97); // set starting y position - random
 
   r = random(0, 255); // set starting red value
   g = random(0, 255); // set starting green value
@@ -55,9 +50,9 @@ function draw() {
   // }
 
   // randomly change color
-  r += random(-1, 1);
-  g += random(-1, 1);
-  b += random(-1, 1);
+  r += random(-10, 10);
+  g += random(-10, 10);
+  b += random(-10, 10);
 
   // don't let values go outside 0-255 range
   r = constrain(r, 0, 255);
@@ -65,19 +60,51 @@ function draw() {
   b = constrain(b, 0, 255);
 
   // if mouse pressed, change color
-  if (mouseIsPressed) {
-    r = random(0, 255);
-    g = random(0, 255);
-    b = random(0, 255);
-    // noStroke();
-    // fill(r, g, b);
-  }
+  // if (mouseIsPressed) {
+  //   r = random(0, 255);
+  //   g = random(0, 255);
+  //   b = random(0, 255);
+  // }
 
-  stroke(r, g, b); // set stroke color
+  //décommenter pour avoir des "ronds"
+  noStroke();
+  fill(r, g, b);
+
+  // décommenter pour avoir des "bulles"
+  // stroke(r, g, b); // set stroke color
   // strokeWeight(100); // set stroke weight
   // noStroke();
 
-  // draw circles
-  circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
-  // circle(mouseX, mouseY, 10); // draw point (mouse)
+  // randomize the number of circles for each draw
+  let n = random(1, 42);
+  for (let i = 0; i < n; i++) {
+    // draw circles
+    circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
+    // circle(mouseX, mouseY, 10); // draw point (mouse)
+  }
+  
+  // circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
+
+  // Wavemaker
+  // // make a x and y grid of ellipses
+  // for (let x = 0; x <= width; x = x + 30) {
+  //   for (let y = 0; y <= height; y = y + 30) {
+  //     // starting point of each circle depends on mouse position
+  //     const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
+  //     const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
+  //     // and also varies based on the particle's location
+  //     const angle = xAngle * (x / width) + yAngle * (y / height);
+
+  //     // each particle moves in a circle
+  //     const myX = x + 20 * cos(2 * PI * t + angle);
+  //     const myY = y + 20 * sin(2 * PI * t + angle);
+
+  //     ellipse(myX, myY, 10); // draw particle
+  //   }
+  // }
+
+  // t = t + 0.01; // update time
 }
+
+// faire plusieurs exemples de <canvas> avec p5js pour github pages
+//if random value then choix1 else choix2 etc...
