170b191ef5c79dfa03a18f48264e675758b518cf
Frederic Gaudreau <gaudreauf@gmail.com>
2023-03-19 13:07:06 -0400

    ouvrir les fenêtres...

--- js/demoWavemaker.js
+++ js/demoWavemaker.js
@@ -1,81 +1,54 @@
-var x;
-var y;
-
-var r;
-var g;
-var b;
-
-let t = 0; // time variable
+let r, g, b;
+let t = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  setStartingColors();
+  background(255);
+}
 
-  r = random(0, 255); // set starting red value
-  g = random(0, 255); // set starting green value
-  b = random(0, 255); // set starting blue value
-
-  background(255); // uncomment pour avoir un fond blanc
-  // background(0); // uncomment pour avoir un fond noir
+function setStartingColors() {
+  r = random(0, 255);
+  g = random(0, 255);
+  b = random(0, 255);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight); // resize canvas to window width and height
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  // randomly change color
-  r += random(-10, 10);
-  g += random(-10, 10);
-  b += random(-10, 10);
+  updateColors();
+  drawWavemaker();
+  t += 0.01;
+}
 
-  // don't let values go outside 0-255 range
-  r = constrain(r, 0, 255);
-  g = constrain(g, 0, 255);
-  b = constrain(b, 0, 255);
+function updateColors() {
+  g = constrain(g + random(-15, 15), 0, 255);
+  b = constrain(b + random(-15, 15), 0, 255);
+  r = constrain(r + random(-15, 15), 0, 255);
+}
 
-  // if mouse pressed, change color
-  // if (mouseIsPressed) {
-  //   r = random(0, 255);
-  //   g = random(0, 255);
-  //   b = random(0, 255);
-  // }
-
-  //décommenter pour avoir des "ronds"
-  noStroke();
-  fill(r, g, b);
-
-  // décommenter pour avoir des "bulles"
-  // stroke(r, g, b); // set stroke color
-  // strokeWeight(100); // set stroke weight
-  // noStroke();
-
-  // randomize the number of circles for each draw
-  //   let n = random(1, 42);
-  //   for (let i = 0; i < n; i++) {
-  //     // draw circles
-  //     circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
-  //     // circle(mouseX, mouseY, 10); // draw point (mouse)
-  //   }
-
-  // circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
-
-  // Wavemaker
-  // make a x and y grid of ellipses
-  for (let x = 0; x <= windowWidth; x = x + 30) {
-    for (let y = 0; y <= windowHeight; y = y + 30) {
-      // starting point of each circle depends on mouse position
+function drawWavemaker() {
+  for (let x = 0; x <= windowWidth; x += 30) {
+    for (let y = 0; y <= windowHeight; y += 30) {
       const xAngle = map(mouseX, 0, windowWidth, -4 * PI, 4 * PI, true);
       const yAngle = map(mouseY, 0, windowHeight, -4 * PI, 4 * PI, true);
-      // and also varies based on the particle's location
       const angle = xAngle * (x / windowWidth) + yAngle * (y / windowHeight);
-
-      // each particle moves in a circle
       const myX = x + 20 * cos(2 * PI * t + angle);
       const myY = y + 20 * sin(2 * PI * t + angle);
-
-      ellipse(myX, myY, 25); // draw particle
+      noStroke();
+      fill(r, g, b);
+      ellipse(myX, myY, 25);
     }
   }
-
-  t = t + 0.01; // update time
 }
+
+function loop() {
+  updateColors();
+  drawWavemaker();
+  t += 0.01;
+  requestAnimationFrame(loop);
+}
+
+loop();
--- js/sketch.js
+++ js/sketch.js
@@ -1,110 +1,34 @@
-var x;
-var y;
-
-var r;
-var g;
-var b;
-
-// let t = 0; // time variable
+let rColor;
+let gColor;
+let bColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  r = random(0, 255); // set starting red value
-  g = random(0, 255); // set starting green value
-  b = random(0, 255); // set starting blue value
-
-  background(255); // uncomment pour avoir un fond blanc
-  // background(0); // uncomment pour avoir un fond noir
+  rColor = random(0, 255);
+  gColor = random(0, 255);
+  bColor = random(0, 255);
+  background(255);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight); // resize canvas to window width and height
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  // var randomValue = random(0, 1);
-  // randomly move lines
-  // if (randomValue < 0.25) {
-  //   x--;
-  // } else if (randomValue < 0.5) {
-  //   x++;
-  // } else if (randomValue < 0.75) {
-  //   y--;
-  // } else {
-  //   y++;
-  // }
+//slow transition between colors
+  rColor = lerp(rColor, random(0, 255), 0.15);
+  gColor = lerp(gColor, random(0, 255), 0.15);
+  bColor = lerp(bColor, random(0, 255), 0.15);
 
-  // // wrap around left and right sides
-  // if (x < 0) {
-  //   x = windowWidth;
-  // } else if (x > windowWidth) {
-  //   x = 0;
-  // }
+  // rColor = random(0, 255);
+  // gColor = random(0, 255);
+  // bColor = random(0, 255);
 
-  // // wrap around top and bottom sides
-  // if (y < 0) {
-  //   y = windowHeight;
-  // } else if (y > windowHeight) {
-  //   y = 0;
-  // }
-
-  // randomly change color
-  r += random(-10, 10);
-  g += random(-10, 10);
-  b += random(-10, 10);
-
-  // don't let values go outside 0-255 range
-  r = constrain(r, 0, 255);
-  g = constrain(g, 0, 255);
-  b = constrain(b, 0, 255);
-
-  // if mouse pressed, change color
-  // if (mouseIsPressed) {
-  //   r = random(0, 255);
-  //   g = random(0, 255);
-  //   b = random(0, 255);
-  // }
-
-  //décommenter pour avoir des "ronds"
   noStroke();
-  fill(r, g, b);
+  fill(rColor, gColor, bColor);
 
-  // décommenter pour avoir des "bulles"
-  // stroke(r, g, b); // set stroke color
-  // strokeWeight(100); // set stroke weight
-  // noStroke();
-
-  // randomize the number of circles for each draw
   let n = random(1, 42);
   for (let i = 0; i < n; i++) {
-    // draw circles
-    circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
-    // circle(mouseX, mouseY, 10); // draw point (mouse)
+    circle(random(windowWidth), random(windowHeight), 25);
   }
-  
-  // circle(random(windowWidth), random(windowHeight), 10); // draw point (random)
-
-  // Wavemaker
-  // // make a x and y grid of ellipses
-  // for (let x = 0; x <= width; x = x + 30) {
-  //   for (let y = 0; y <= height; y = y + 30) {
-  //     // starting point of each circle depends on mouse position
-  //     const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-  //     const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
-  //     // and also varies based on the particle's location
-  //     const angle = xAngle * (x / width) + yAngle * (y / height);
-
-  //     // each particle moves in a circle
-  //     const myX = x + 20 * cos(2 * PI * t + angle);
-  //     const myY = y + 20 * sin(2 * PI * t + angle);
-
-  //     ellipse(myX, myY, 10); // draw particle
-  //   }
-  // }
-
-  // t = t + 0.01; // update time
 }
-
-// faire plusieurs exemples de <canvas> avec p5js pour github pages
-//if random value then choix1 else choix2 etc...
